ce339cdcbae35937d99db25cfdf57aed654bd619
rednt <47644820+rednt@users.noreply.github.com>
2025-01-23 13:30:18 +0100

    added addNote activity

--- app/src/main/java/com/example/notesapp/Note.java
+++ app/src/main/java/com/example/notesapp/Note.java
@@ -1,41 +1,27 @@
 package com.example.notesapp;
 
-import android.content.Intent;
+import android.app.ActionBar;
 import android.os.Bundle;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class Note extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.addNote);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Note.class));
-            }
-
-    });
-
     }
+
 }
